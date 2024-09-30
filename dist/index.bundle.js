d95a22ef7e6077506d1b722d5dd969a97622da9c
Alex Yagodkin <adamsHillzzz@gmail.com>
2024-09-30 07:27:28 +0300

    feat: update template

---
+++ dist/index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello, World!');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYnBhY2stdGVtcGxhdGUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0hlbGxvLCBXb3JsZCEnKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	mode: env.mode ?? 'development',
+	entry: {
+		index: './src/index.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Development',
+		}),
+	],
 	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'build'),
+		clean: true,
 	},
 };
